15029181d4af0585c0a097bc3d6fcb8739de6257
Ryota <disneyresort1983@icloud.com>
2023-04-10 17:15:38 +0900

    ユーザーログイン、新規登録機能

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  get '/login', to: 'user_sessions#new', as: :login
+  post '/login', to: 'user_sessions#create'
+  delete '/logout', to: 'user_sessions#destroy', as: :logout
+  resources :users, only: [:new, :create, :edit, :update, :destroy]
 end

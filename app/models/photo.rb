90b16f2459edfc22fb07e79f7ca6ea325707be28
Ryota <disneyresort1983@icloud.com>
2023-05-07 22:38:18 +0900

    写真投稿フォーム修正

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -1,22 +1,10 @@
 class Photo < ApplicationRecord
     belongs_to :user
-    has_one_attached :image
+    mount_uploader :image, PhotoImageUploader
     
     validates :day, presence: true
     validates :park, presence: true
     validates :area, presence: true
     validates :user_id, presence: true
-
-    geocoded_by :park
-    after_validation :geocode, if: :park_changed?
-
-    def image_url
-        image.attached? ? image.blob.service_url : nil
-    end
-
-    def image_url=(url)
-        self.image.attach(io: File.open(url), filename: "image.jpg")
-    end
-
 end
 
--- db/migrate/20230429122012_add_not_null_photos_area.rb
+++ db/migrate/20230429122012_add_not_null_photos_area.rb
@@ -1,5 +1,7 @@
 class AddNotNullPhotosArea < ActiveRecord::Migration[6.1]
   def change
     change_column_null :photos, :area, false
+    change_column_null :photos, :latitude, false
+    change_column_null :photos, :longitude, false
   end
 end

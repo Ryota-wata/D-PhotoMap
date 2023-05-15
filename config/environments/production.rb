c763a2c3261b67c64dcb2aa3265809047fb618b7
Ryota <disneyresort1983@icloud.com>
2023-05-15 14:00:38 +0900

    インデント修正

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -119,4 +119,4 @@ Rails.application.configure do
   # config.active_record.database_resolver_context = ActiveRecord::Middleware::DatabaseSelector::Resolver::Session
  
   config.action_mailer.default_url_options = Settings.default_url_options.to_h
- 
+ end

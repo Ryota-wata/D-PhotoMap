7f04a6edb56f05995c16483fdd0d24d64aae7fa4
Ryota <disneyresort1983@icloud.com>
2023-05-16 13:56:40 +0900

    利用規約、プライバシーポリシー作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
   mount LetterOpenerWeb::Engine, at: '/letter_opener' if Rails.env.development?
   root 'static_pages#top'
-  get '/privacy', to: 'static_pages#privacy'
-  get '/terms', to: 'static_pages#terms'
+  get '/privacy_policy', to: 'static_pages#privacy_policy'
+  get '/terms_of_use', to: 'static_pages#terms_of_use'
   get '/login', to: 'user_sessions#new', as: :login
   post '/login', to: 'user_sessions#create'
   delete '/logout', to: 'user_sessions#destroy', as: :logout

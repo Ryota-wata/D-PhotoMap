fc57a55717712d88ee7a4f70e67a2b28b88d1723
Ryota <disneyresort1983@icloud.com>
2023-05-28 21:13:13 +0900

    twitter認証機能修正

--- app/controllers/oauths_controller.rb
+++ app/controllers/oauths_controller.rb
@@ -7,18 +7,25 @@ class OauthsController < ApplicationController
 
   def callback
     provider = auth_params[:provider]
-    if auth_params[:denied].present? 
-      redirect_to root_path, success: 'ログインをキャンセルしました'
+
+    if auth_params[:denied].present?
+      redirect_to root_path, success: ('Twitterアカウントでのアカウント作成に成功しました')
       return
     end
-    create_user_from(provider) unless (@user = login_from(provider))
-    redirect_to root_path, success: "#{provider.titleize}でログインしました"
+
+    begin
+      create_user_from(provider) unless (@user = login_from(provider))
+      redirect_to photos_path, success: ('Twitterアカウントでのログインに成功しました')
+ 
+    rescue StandardError
+      redirect_to root_path, danger: ('Twitterアカウントでログインに失敗しました')
+    end
   end
 
   private
 
   def auth_params
-    params.permit(:code, :provider, :denied)
+    params.permit(:code, :provider, :denied, :oauth_token, :oauth_verifier)
   end
 
   def create_user_from(provider)
